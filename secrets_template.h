b84ab85d7a1d7ae95f3bc7f6352db0df48608b72
Andrew Morty <mortyone@hotmail.com>
2025-06-08 09:08:55 +0100

    Create secrets_template.h

---
+++ secrets_template.h
@@ -0,0 +1,12 @@
+#pragma once
+
+const char* hostname     = "espSolarChargerOTA";
+const char* mqtt_user    = "mqtt_user";
+const char* mqtt_pass    = "mqtt_password";
+const char* mqtt_server  = "192.168.1.15";
+const int   mqtt_port    = 1883;
+const char* wifi_ssid    = "wifi_network";
+const char* wifi_pass    = "wifi_passsword";
+const char* mqtt_topic = "epever/state";
+const char* ota_pass     = "guessme";
+const char* mqtt_deepsleep = "epever/deepsleep";
